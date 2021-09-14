abc5562f9b716183345dff6e0ca2663b137d8e08
FabianGuevaraPinilla <fabianguevara157@gmail.com>
2021-09-13 20:24:35 -0500

    config app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-// Se trae la librería de express
-const express = require('express');
+/**
+ * Archivo de arranque del proyecto
+ */
+
+// Módulos necesarios
+const express = require('express'); // Se trae la librería de express
+
+
+// Se crea la aplicación express
 const app = express();
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 //se recomienda usar el puerto especificado como una variable de entorno
 app.set('puerto', process.env.PORT || 3000);
+
 app.listen(app.get('puerto'), function () {
   console.log('Example app listening on port ' + app.get('puerto') + '!');
 });
